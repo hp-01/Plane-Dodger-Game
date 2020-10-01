977792e2012ca379b6ec146f1b78f47aeac9f3e1
Pandey Harsh Sunil <harshpandey971@gmail.com>
2020-10-01 18:31:15 +0530

    Update BaseActor.java

--- src/game/BaseActor.java
+++ src/game/BaseActor.java
@@ -3,11 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.Array;
 
 /**
  *
@@ -15,13 +22,80 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
  */
 public class BaseActor extends Actor
 {
-    private Animation<Textureegion
-    public BaseActor(float x,float y,Stage s)
+
+    private Animation<TextureRegion> animation;
+    private float elapsedTime;
+    private boolean animationPaused;
+
+    public BaseActor(float x, float y, Stage s)
       {
         // call constructor from Actor class
         super();
         // perform additional initialization tasks
-        setPosition(x,y);
-        s.addActor(this);  
+        setPosition(x, y);
+        s.addActor(this);
+        animation = null;
+        elapsedTime = 0;
+        animationPaused = false;
+      }
+
+    private void setAnimation(Animation<TextureRegion> anim)
+      {
+        animation = anim;
+        TextureRegion tr = animation.getKeyFrame(0);
+        float w = tr.getRegionWidth();
+        float h = tr.getRegionHeight();
+        setSize(w,h);
+        setOrigin(w/2,h/2);
+      }
+
+    public void setAnimationPaused(boolean pause)
+      {
+        animationPaused = pause;
+      }
+
+    public Animation<TextureRegion> loadAimationFromFiles(String[] fileNames, float frameDuration, boolean loop)
+      {
+        int fileCount = fileNames.length;
+        Array<TextureRegion> textureArray = new Array<TextureRegion>();
+
+        for (int n = 0; n < fileCount; n++)
+        {
+            String fileName = fileNames[n];
+            Texture texture = new Texture(Gdx.files.internal(fileName));
+            texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+            textureArray.add(new TextureRegion(texture));
+        }
+
+        Animation<TextureRegion> anim = new Animation<TextureRegion>(frameDuration, textureArray);
+
+        if (loop)
+        {
+            anim.setPlayMode(Animation.PlayMode.LOOP);
+        } else
+        {
+            anim.setPlayMode(Animation.PlayMode.NORMAL);
+        }
+
+        if (animation == null)
+        {
+            setAnimation(anim);
+        }
+        return anim;
+      }
+
+    public void draw(Batch batch, float parentAlpha)
+      {
+        super.draw(batch, parentAlpha);
+
+        // apply color tint effect
+        Color c = getColor();
+        batch.setColor(c.r, c.g, c.b, c.a);
+
+        if (animation != null && isVisible())
+        {
+            batch.draw(animation.getKeyFrame(elapsedTime), getX(), getY(), getOriginX(), getOriginY(), getWidth(),
+                    getHeight(), getScaleX(), getScaleY(), getRotation());
+        }
       }
 }
