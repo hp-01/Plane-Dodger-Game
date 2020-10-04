9af30055fb712e70a493a76317964fdd35409048
Pandey Harsh Sunil <harshpandey971@gmail.com>
2020-10-04 20:33:48 +0530

    Update BaseGame.java
    Textbutton style for textbutton throughout the game.

--- src/game/BaseGame.java
+++ src/game/BaseGame.java
@@ -12,9 +12,12 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 
 /**
  *
@@ -25,6 +28,7 @@ public abstract class BaseGame extends Game
 
     private static BaseGame game;
     public static LabelStyle labelStyle;
+    public static TextButtonStyle textButtonStyle;
 
     public BaseGame()
       {
@@ -59,5 +63,13 @@ public abstract class BaseGame extends Game
         
         BitmapFont customFont  = fontGenerator.generateFont(fontParameters);
         labelStyle.font = customFont;
-      }
+        
+        //textbutton style
+        textButtonStyle = new TextButtonStyle();
+        Texture buttonTex = new Texture(Gdx.files.internal("assets/button.png"));
+        NinePatch buttonPatch = new NinePatch(buttonTex, 24,24,24,24);
+        textButtonStyle.up = new NinePatchDrawable(buttonPatch);
+        textButtonStyle.font = customFont;
+        textButtonStyle.fontColor = Color.GRAY;
+    }
 }
