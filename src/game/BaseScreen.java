8480bedf79eb49ee6cd9663cde2d02c2f0d68ebb
Pandey Harsh Sunil <harshpandey971@gmail.com>
2020-10-04 20:36:39 +0530

    Update BaseScreen.java
    Added touchDown method.

--- src/game/BaseScreen.java
+++ src/game/BaseScreen.java
@@ -10,6 +10,9 @@ import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputEvent.Type;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 /**
@@ -55,6 +58,11 @@ public abstract class BaseScreen implements Screen, InputProcessor
         im.addProcessor(this);
         im.addProcessor(mainStage);
         im.addProcessor(uiStage);
+      }
+
+    public boolean isTouchDownEvent(Event e)
+      {
+        return (e instanceof InputEvent) && ((InputEvent) e).getType().equals(Type.touchDown);
       }
 
     @Override
