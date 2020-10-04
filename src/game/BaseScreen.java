309ef4f24ae9fc6d7172901b11f9875e27148ebb
Pandey Harsh Sunil <harshpandey971@gmail.com>
2020-10-04 13:32:26 +0530

    Update BaseScreen.java
    Added InputProcessor and InputMultiplexer for discrete input.

--- src/game/BaseScreen.java
+++ src/game/BaseScreen.java
@@ -6,6 +6,8 @@
 package game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -14,7 +16,7 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
  *
  * @author Harsh Pandey
  */
-public abstract class BaseScreen implements Screen
+public abstract class BaseScreen implements Screen, InputProcessor
 {
 
     protected Stage mainStage;
@@ -49,7 +51,10 @@ public abstract class BaseScreen implements Screen
     @Override
     public void show()
       {
-
+        InputMultiplexer im = (InputMultiplexer)Gdx.input.getInputProcessor();
+        im.addProcessor(this);
+        im.addProcessor(mainStage);
+        im.addProcessor(uiStage);
       }
 
     @Override
@@ -81,6 +86,54 @@ public abstract class BaseScreen implements Screen
       {
         mainStage.dispose();
         uiStage.dispose();
+      }
+
+    @Override
+    public boolean keyDown(int keycode)
+      {
+        return false;
+      }
+
+    @Override
+    public boolean keyUp(int keycode)
+      {
+        return false;
+      }
+
+    @Override
+    public boolean keyTyped(char character)
+      {
+        return false;
+      }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button)
+      {
+        return false;
+      }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button)
+      {
+        return false;
+      }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer)
+      {
+        return false;
+      }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY)
+      {
+        return false;
+      }
+
+    @Override
+    public boolean scrolled(int amount)
+      {
+        return false;
       }
 
 }
