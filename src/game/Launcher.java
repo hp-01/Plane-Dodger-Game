009b6a8a904b7b77490302869ec790820b49693d
Pandey Harsh Sunil <harshpandey971@gmail.com>
2020-10-04 13:46:38 +0530

    Create Launcher.java

---
+++ src/game/Launcher.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+
+/**
+ *
+ * @author Harsh Pandey
+ */
+public class Launcher
+{
+    public static void main(String[] args)
+      {
+        Game myGame = new MainGame();
+        LwjglApplication launcher = new LwjglApplication(myGame,"Main Game",800,600);
+      }
+}
