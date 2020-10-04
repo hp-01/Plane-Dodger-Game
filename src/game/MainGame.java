d25220a92d82bbb78c52a2029296a4bb3cabf2d2
Pandey Harsh Sunil <harshpandey971@gmail.com>
2020-10-04 13:41:22 +0530

    Create MainGame.java

---
+++ src/game/MainGame.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+/**
+ *
+ * @author Harsh Pandey
+ */
+public class MainGame extends BaseGame
+{
+
+    @Override
+    public void create()
+      {
+        super.create();
+        this.setScreen(new LevelScreen());
+      }
+
+    @Override
+    public void dispose()
+      {
+        super.dispose();
+      }
+
+}
