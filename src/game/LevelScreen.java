59218d04eda0ce230a1632d943a194f89ae66df5
Pandey Harsh Sunil <harshpandey971@gmail.com>
2020-10-05 12:22:44 +0530

    Correct

--- src/game/LevelScreen.java
+++ src/game/LevelScreen.java
@@ -8,6 +8,7 @@ package game;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 
 /**
@@ -18,6 +19,12 @@ public class LevelScreen extends BaseScreen
 {
 
     Plane plane;
+
+    float startTimer;
+    float starSpawnInterval;
+    int score;
+    Label scoreLabel;
+
     @Override
     public void initialize()
       {
@@ -25,23 +32,52 @@ public class LevelScreen extends BaseScreen
         new Sky(800, 0, mainStage);
         new Ground(0, 0, mainStage);
         new Ground(800, 0, mainStage);
-        
+
         // plane onto mainStage
-        plane = new Plane(100,500,mainStage);
-        BaseActor.setWorldBounds(800,600);
+        plane = new Plane(100, 500, mainStage);
+        BaseActor.setWorldBounds(800, 600);
+
+        startTimer = 0;
+        starSpawnInterval = 4;
+        score = 0;
+        scoreLabel = new Label(Integer.toString(score), BaseGame.labelStyle);
+        uiTable.pad(10);
+        uiTable.add(scoreLabel);
+        uiTable.row();
+        uiTable.add().expandY();
       }
 
     @Override
     public void update(float dt)
       {
-         
+        startTimer += dt;
+        
+        // to spawn new star at around 4 second interval
+        if(startTimer > starSpawnInterval)
+        {
+            new Star(800, MathUtils.random(100, 500), mainStage);
+            startTimer = 0;
+        }
+        
+        // to detect collision with star
+        for(BaseActor star : BaseActor.getList(mainStage, "Star"))
+        {
+            if(plane.overlaps(star))
+            {
+                star.remove();
+                score++;
+                scoreLabel.setText(Integer.toString(score));
+            }
+        }
       }
 
     @Override
     public boolean keyDown(int keyCode)
       {
-        if(keyCode == Keys.SPACE || keyCode == Keys.UP)
+        if (keyCode == Keys.SPACE || keyCode == Keys.UP)
+        {
             plane.boost();
+        }
         return true;
       }
 }
