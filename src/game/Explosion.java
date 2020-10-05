1596488fbd45980fc9fe407fc2044e0cb84d8854
Pandey Harsh Sunil <harshpandey971@gmail.com>
2020-10-05 13:57:17 +0530

    Update Explosion.java

--- src/game/Explosion.java
+++ src/game/Explosion.java
@@ -6,11 +6,27 @@
 
 package game;
 
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
 /**
  *
  * @author Harsh Pandey
  */
-public class Explosion
+public class Explosion extends BaseActor
 {
+
+    public Explosion(float x, float y, Stage s)
+      {
+        super(x, y, s);
+        loadAnimationFromSheet("assets/explosion.png",6,6,0.02f,false);
+      }
+    
+    public void act(float dt)
+      {
+        super.act(dt);
+        
+        if(isAnimationFinished())
+            remove();
+      }
     
 }
