1dea96a6630afe828a7959be93ed90b61d0e6427
Pandey Harsh Sunil <harshpandey971@gmail.com>
2020-10-08 21:46:48 +0530

    Added background music

--- src/game/MenuScreen.java
+++ src/game/MenuScreen.java
@@ -6,9 +6,16 @@
 package game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Event;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 /**
  *
@@ -22,41 +29,55 @@ public class MenuScreen extends BaseScreen
     @Override
     public void initialize()
       {
-            new Sky(0, 0, mainStage);
-            new Sky(800, 0, mainStage);
-            new Ground(0, 0, mainStage);
-            new Ground(800, 0, mainStage);
+        new Sky(0, 0, mainStage);
+        new Sky(800, 0, mainStage);
+        new Ground(0, 0, mainStage);
+        new Ground(800, 0, mainStage);
 
-            Label title = new Label("Plane Dodger", BaseGame.labelStyle);
-            title.setFontScale((float) 1.5);
-            uiTable.add(title).colspan(2).pad(50);
+        Label title = new Label("Plane Dodger", BaseGame.labelStyle);
+        title.setFontScale((float) 1.5);
+        title.setColor(Color.ORANGE);
+        uiTable.add(title).colspan(2).pad(50);
 
-            TextButton startButton = new TextButton("Start", BaseGame.textButtonStyle);
-            startButton.addListener((Event e) ->
+        Music backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("assets/Prelude-and-Action.mp3"));
+        backgroundMusic.setLooping(true);
+        backgroundMusic.setVolume(1.00f);
+        backgroundMusic.play();
+
+        TextButton startButton = new TextButton("Start", BaseGame.textButtonStyle);
+        startButton.addListener((Event e) ->
+        {
+            if (!isTouchDownEvent(e))
             {
-                if (!isTouchDownEvent(e))
-                {
-                    return false;
-                }
-                MainGame.setActiveScreen(new LevelScreen());
-                return true;
-            });
+                return false;
+            }
+            backgroundMusic.stop();
+            backgroundMusic.dispose();
+            MainGame.setActiveScreen(new LevelScreen());
+            return true;
+        });
 
-            TextButton quitButton = new TextButton("Quit", BaseGame.textButtonStyle);
-            quitButton.addListener(
-                    (Event e) ->
+        TextButton quitButton = new TextButton("Quit", BaseGame.textButtonStyle);
+        quitButton.addListener(
+                (Event e) ->
+                {
+                    if (!isTouchDownEvent(e))
                     {
-                        if (!isTouchDownEvent(e))
-                        {
-                            return false;
-                        }
-                        Gdx.app.exit();
-                        return true;
-                    });
-            uiTable.row();
-            uiTable.add(startButton).expandX();
-            uiTable.add(quitButton).expandX();
-            
+                        return false;
+                    }
+                    Gdx.app.exit();
+                    return true;
+                });
+        uiTable.row();
+        uiTable.add(startButton).expandX();
+        uiTable.add(quitButton).expandX();
+
+        String[] score = Gdx.files.internal("assets/main.dd").readString().split("\n");
+        Label scoreLabel = new Label("High Score: " + score[0], BaseGame.labelStyle);
+
+        uiTable.row();
+        uiTable.add(scoreLabel).colspan(2).pad(20);
+
       }
 
     @Override
