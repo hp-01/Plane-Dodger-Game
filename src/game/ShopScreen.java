7e6b2bab48b031edd3cf9c8345c03bfe92af8891
Pandey Harsh Sunil <harshpandey971@gmail.com>
2020-10-12 18:26:02 +0530

    Create ShopScreen.java

---
+++ src/game/ShopScreen.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package game;
+
+/**
+ *
+ * @author Harsh Pandey
+ */
+public class ShopScreen extends BaseScreen
+{
+
+    @Override
+    public void initialize()
+      {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      }
+
+    @Override
+    public void update(float dt)
+      {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      }
+    
+}
